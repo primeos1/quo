85c2f388d6385ea119e800f76c1c794f8b8fd678
primeos1 <akhenisio@yahoo.com>
2020-08-19 00:43:00 +0100

    Updates

---
+++ precache-manifest.ab063613cc095c88da9fbb5e2d1ea816.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f46ab58a2c855e7886753fce85b8c1",
+    "url": "/quo/index.html"
+  },
+  {
+    "revision": "0a2db4b0a04576cdae4e",
+    "url": "/quo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3ae098ebd70544127b52",
+    "url": "/quo/static/css/main.9d35c21d.chunk.css"
+  },
+  {
+    "revision": "0a2db4b0a04576cdae4e",
+    "url": "/quo/static/js/2.f0c25e32.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/quo/static/js/2.f0c25e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ae098ebd70544127b52",
+    "url": "/quo/static/js/main.f378413d.chunk.js"
+  },
+  {
+    "revision": "1d0adefbe21e4a30d545",
+    "url": "/quo/static/js/runtime-main.ec4ff420.js"
+  }
+]);
